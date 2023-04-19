d409fa2d6f61965923c890af717c4860c15ba690
walidfajraoui <w.fajraoui.ext@lesaffre.com>
2023-04-19 15:30:09 +0100

    add machine-piece

--- src/machine-pieces/dto/update-machine-piece.dto.ts
+++ src/machine-pieces/dto/update-machine-piece.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateMachinePieceDto } from './create-machine-piece.dto';
 
 export class UpdateMachinePieceDto extends PartialType(CreateMachinePieceDto) {}
--- src/machine-pieces/machine-pieces.controller.ts
+++ src/machine-pieces/machine-pieces.controller.ts
@@ -6,38 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { MachinePiecesService } from './machine-pieces.service';
 import { CreateMachinePieceDto } from './dto/create-machine-piece.dto';
 import { UpdateMachinePieceDto } from './dto/update-machine-piece.dto';
 import { ApiTags } from '@nestjs/swagger';
-@ApiTags('machine-pices')
-@Controller('machine-pices')
+@ApiTags('machine-pieces')
+@Controller('machine-peices')
 export class MachinePicesController {
   constructor(private readonly machinePicesService: MachinePiecesService) {}
-
   @Post()
-  create(@Body() createMachinePiceDto: CreateMachinePieceDto) {
-    return this.machinePicesService.create(createMachinePiceDto);
+  create(@Body() createMachineDto: CreateMachinePieceDto) {
+    return this.machinePicesService.create(createMachineDto);
   }
-
   @Get()
-  findAll() {
-    return this.machinePicesService.findAll();
+  find(@Query() query) {
+    return this.machinePicesService.find(query);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.machinePicesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateMachinePiceDto: UpdateMachinePieceDto,
-  ) {
-    return this.machinePicesService.update(+id, updateMachinePiceDto);
-  }
+  // @Patch(':id')
+  // update(
+  //   @Param('id') id: string,
+  //   @Body() updateMachinePiceDto: UpdateMachinePieceDto,
+  // ) {
+  //   return this.machinePicesService.update(+id, updateMachinePiceDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
