710240f10015d0d5bdbf3a41232254dfd5f2d877
houssemBen <houssembmouelli@gmail.com>
2023-06-21 00:16:49 +0100

    feat: Revision et amelioration des différants champs des forms (module profile)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,6 +31,7 @@ export class AuthService {
       {
         email,
         relations: [
+          'teams',
           'role',
           'role.rolePermissions',
           'role.rolePermissions.permission',
