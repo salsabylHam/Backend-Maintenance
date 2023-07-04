5d4a61257f54b5e562fdf53a490b845b5f7902c7
houssemBen <houssembmouelli@gmail.com>
2023-07-04 09:35:12 +0100

    add password to swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,15 +14,28 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const config = new DocumentBuilder()
-    .setTitle('Maintenance')
-    .setDescription('The maintenance API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addTag('Maintenance')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/docs', app, document);
+
+  if (process.env.ENV != 'prod' && process.env.ENV != 'production') {
+    app.use(
+      ['/api/v1/docs'],
+      basicAuth({
+        challenge: true,
+        users: {
+          [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+        },
+      }),
+    );
+    const config = new DocumentBuilder()
+      .setTitle('Maintenance')
+      .setDescription('The maintenance API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addTag('Maintenance')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/docs', app, document);
+  }
+
   await app.listen(4000);
 }
 bootstrap();
