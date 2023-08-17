d5586577d0220b6b30aa4e679d3e279659d6931e
houssemBen <houssembmouelli@gmail.com>
2023-08-17 17:18:31 +0100

    fix: change synchronize option for database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -14,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
           password: config.get('DB_PASSWORD'),
           database: config.get('DB_NAME'),
           port: config.get('DB_PORT'),
-          synchronize: true,
+          synchronize: false,
           autoLoadEntities: true,
           migrationsRun: true,
           //logging: 'all',
